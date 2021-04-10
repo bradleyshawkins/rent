55c5a2a6e85af4a8b71830796db9d351c9b4b33e
Brad Hawkins <brad.hawkins@getweave.com>
2021-04-10 16:20:02 -0600

    Add config package

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	ConnectionString string `envconfig:"DB_CONNECTIONSTRING" default:"root:password@tcp(localhost:3306)/rent"`
+	MigrationPath    string `envconfig:"DB_MIGRATION_PATH" default:"../../mysql/schema"`
+}
+
+func ParseConfig() (*Config, error) {
+	log.Println("Parsing config")
+	var c Config
+	err := envconfig.Process("", &c)
+	if err != nil {
+		return nil, fmt.Errorf("unable to process config. Error:%v", err)
+	}
+	return &c, nil
+}
