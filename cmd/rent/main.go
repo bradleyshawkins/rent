4db10dca7f6bc0c100df2cb169bca7e695545cf2
Brad Hawkins <brad.hawkins@getweave.com>
2021-04-26 20:52:29 -0600

    Add postgres connection back in.

--- cmd/rent/main.go
+++ cmd/rent/main.go
@@ -5,22 +5,23 @@ import (
 	"log"
 	"os"
 
+	"github.com/bradleyshawkins/rent/postgres"
+
 	"github.com/bradleyshawkins/rent/config"
 )
 
 func main() {
 	log.Println("Starting rent service")
-	_, err := config.ParseConfig()
+	c, err := config.ParseConfig()
 	if err != nil {
 		log.Println(fmt.Errorf("unable to initialize config. Error: %v", err))
 		os.Exit(0)
 	}
-	//
-	//m, err := postgres.New(c.ConnectionString, c.MigrationPath)
-	//if err != nil {
-	//	log.Printf("unable to get database connection. Error: %v\n", err)
-	//	os.Exit(1)
-	//}
+	_, err = postgres.New(c.ConnectionString, c.MigrationPath)
+	if err != nil {
+		log.Printf("unable to get database connection. Error: %v\n", err)
+		os.Exit(1)
+	}
 	//
 	//personService := person.NewPersonService(m)
 	//
