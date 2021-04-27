ab98e61eee7c9ef1d7bc8bac3f566e0da83273b2
Brad Hawkins <brad.hawkins@getweave.com>
2021-04-26 20:49:55 -0600

    Remove stuff for build

--- cmd/rent/main.go
+++ cmd/rent/main.go
@@ -1,42 +1,31 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"os"
-
-	"github.com/bradleyshawkins/rent/postgres"
-
-	"github.com/bradleyshawkins/rent/config"
-
-	"github.com/bradleyshawkins/rent/person"
-
-	"github.com/bradleyshawkins/rent/http"
 )
 
 func main() {
 	log.Println("Starting rent service")
-	c, err := config.ParseConfig()
-	if err != nil {
-		log.Println(fmt.Errorf("unable to initialize config. Error: %v", err))
-		os.Exit(0)
-	}
-
-	m, err := postgres.New(c.ConnectionString, c.MigrationPath)
-	if err != nil {
-		log.Printf("unable to get database connection. Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	personService := person.NewPersonService(m)
-
-	router := http.SetupRouter(personService)
-
-	if err := router.Start(context.Background(), ":8080"); err != nil {
-		log.Println("unable to start router. Error:", err)
-		os.Exit(2)
-	}
+	//c, err := config.ParseConfig()
+	//if err != nil {
+	//	log.Println(fmt.Errorf("unable to initialize config. Error: %v", err))
+	//	os.Exit(0)
+	//}
+	//
+	//m, err := postgres.New(c.ConnectionString, c.MigrationPath)
+	//if err != nil {
+	//	log.Printf("unable to get database connection. Error: %v\n", err)
+	//	os.Exit(1)
+	//}
+	//
+	//personService := person.NewPersonService(m)
+	//
+	//router := http.SetupRouter(personService)
+	//
+	//if err := router.Start(context.Background(), ":8080"); err != nil {
+	//	log.Println("unable to start router. Error:", err)
+	//	os.Exit(2)
+	//}
 
 	log.Println("Ready for traffic")
 }
