a2e3b4bcbf062a6834776c24b31f863f3789e8ea
Brad Hawkins <bradleyshawkins@gmail.com>
2021-09-06 19:28:02 -0600

    Remove unused sql and rename file (#6)
    Co-authored-by: Brad Hawkins <brad.hawkins@getweave.com>

--- postgres/schema/0001_person.sql
+++ postgres/schema/0001_person.sql
@@ -30,32 +30,6 @@ CREATE TABLE person (
 
 CREATE UNIQUE INDEX user_username_idx ON person(email_address);
 
-
-CREATE TABLE IF NOT EXISTS account_status(
-    id INT NOT NULL PRIMARY KEY,
-    title VARCHAR(20) NOT NULL
-);
-
-INSERT INTO account_status(id, title) VALUES(1, 'active');
-INSERT INTO account_status(id, title) VALUES(2, 'inactive');
-INSERT INTO account_status(id, title) VALUES(3, 'disabled');
-
-CREATE TABLE IF NOT EXISTS account(
-    id UUID NOT NULL PRIMARY KEY,
-    status_id INT NOT NULL REFERENCES account_status(id),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS membership(
-    person_id UUID NOT NULL REFERENCES person(id),
-    account_id UUID NOT NULL REFERENCES account(id),
-    permission INT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (person_id, account_id)
-);
-
 -- +goose StatementBegin
 CREATE OR REPLACE FUNCTION update_modified_at_column()
     RETURNS TRIGGER AS $$
@@ -68,17 +42,13 @@ $$ language plpgsql;
 
 CREATE TRIGGER update_person_modified_at_column BEFORE INSERT OR UPDATE ON person FOR EACH ROW EXECUTE FUNCTION update_modified_at_column();
 CREATE TRIGGER update_person_details_modified_at_column BEFORE INSERT OR UPDATE ON person_details FOR EACH ROW EXECUTE FUNCTION update_modified_at_column();
-CREATE TRIGGER update_account_modified_at_column BEFORE INSERT OR UPDATE ON account FOR EACH ROW EXECUTE FUNCTION update_modified_at_column();
-CREATE TRIGGER update_membership_modified_at_column BEFORE INSERT OR UPDATE ON membership FOR EACH ROW EXECUTE FUNCTION update_modified_at_column();
 
 -- +goose Down
-DROP TRIGGER IF EXISTS update_membership_modified_at_column ON membership;
-DROP TRIGGER IF EXISTS update_account_modified_at_column ON account;
 DROP TRIGGER IF EXISTS update_person_details_modified_at_column ON person_details;
 DROP TRIGGER IF EXISTS update_person_modified_at_column ON person;
 
 DROP FUNCTION update_modified_at_column();
 
-DROP TABLE account;
-DROP TABLE person_details;
 DROP TABLE person;
+DROP TABLE person_details;
+DROP TABLE person_status;
